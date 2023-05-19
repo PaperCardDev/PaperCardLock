45cd405d80899f27dea8b26e0578219535f061ca
盼 <1398872296@qq.com>
2023-05-19 20:36:55 +0800

    初始提交

---
+++ src/main/java/cn/paper_card/papercardlock/PaperCardLock.java
@@ -0,0 +1,17 @@
+package cn.paper_card.papercardlock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PaperCardLock extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
